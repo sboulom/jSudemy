a3aa1aa9546e8c12c0bb6fdb2da307dbc8f9125a
Steven Boulom <sboulom@gmail.com>
2021-02-05 09:31:56 -0800

    js practice

--- assignments.js
+++ assignments.js
@@ -1,89 +1,105 @@
-const country = "United States";
-const continent = "North America";
-const population = 80;
+// const country = "United States";
+// const continent = "North America";
+// const population = 80;
 
-console.log(country);
-console.log(continent);
-console.log(population);
-const isIsland = false;
-// isIsland = true;
-let language = "English";
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+// const isIsland = false;
+// // isIsland = true;
+// let language = "English";
 
-// console.log(typeof isIsland);
-// console.log(typeof population);
-// console.log(typeof country);
-// console.log(typeof language);
+// // console.log(typeof isIsland);
+// // console.log(typeof population);
+// // console.log(typeof country);
+// // console.log(typeof language);
 
-// console.log(population/2);
-// population++;
+// // console.log(population/2);
+// // population++;
 
-// temperate literal 
+// // temperate literal 
 
-const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+// const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
 
-console.log(description);
+// console.log(description);
 
 
-// if else 
-if(population > 33){
-    console.log(`${country}'s population is above average`);
-} else {
-    console.log(`${country}'s populstion is below average`);
+// // if else 
+// if(population > 33){
+//     console.log(`${country}'s population is above average`);
+// } else {
+//     console.log(`${country}'s populstion is below average`);
+// }
+
+// // type conversion and coercion 
+
+// console.log('9' - '5');
+// console.log('19' - '13'+ '17');
+// console.log('19' - '13'+ 17);
+// console.log('123' < 57);
+// console.log(5 + 6 + '4' + 9 - 4 - 2);
+
+// // let numNeighbors = prompt (
+// //     "How many neigbor countries does your country have?"
+// // )
+
+// // if (numNeighbors === 1 ){
+// //     console.log("only one border");
+// // } else if (numNeighbors > 1){
+// //     console.log("more than 1 border");
+// // } else {
+// //     console.log("its an islan bruh")
+// // }
+
+// if (language === "English" && population < 50 && !isIsland ){
+//     console.log(`you should live in ${country}`)
+// } else {
+//     console.log("find another country")
+// }
+
+
+// // switch 
+
+// // switch (language) {
+// //     case mandarin:
+// //         console.log("Most number of native speakers!");
+// //         break;
+// //     case spanish:
+// //         console.log("2nd place in number of native speakers");
+// //         break;
+// //     case english: 
+// //         console.log("3rd place");
+// //         break;
+// //     case hindi:
+// //         console.log("number 4");
+// //         break;
+// //     case arabic:
+// //         console.log("5th most spoken language");
+// //         break
+// //     default:
+// //         console.log("time to learn a language");
+// // }
+
+// // ternary operator 
+// // if(`${population}` > 33){ 
+// //     console.log(`${country}'s populaton is above average`);
+// // } else {
+// //     console.log(`${country}'s populaton is below average`);
+// // }
+
+function describeCountry(country, population, capitalCity){
+    return `${country} has ${population} million people and its capital city is ${capitalCity}`;
 }
 
-// type conversion and coercion 
+const descPortugal = describeCountry('Portugal', 10, 'Lisbon');
+const descGermany = describeCountry('Germany', 83, 'Berlin');
+const descFinland = describeCountry('Finland', 6, 'Helsinki');
 
-console.log('9' - '5');
-console.log('19' - '13'+ '17');
-console.log('19' - '13'+ 17);
-console.log('123' < 57);
-console.log(5 + 6 + '4' + 9 - 4 - 2);
-
-// let numNeighbors = prompt (
-//     "How many neigbor countries does your country have?"
-// )
-
-// if (numNeighbors === 1 ){
-//     console.log("only one border");
-// } else if (numNeighbors > 1){
-//     console.log("more than 1 border");
-// } else {
-//     console.log("its an islan bruh")
-// }
-
-if (language === "English" && population < 50 && !isIsland ){
-    console.log(`you should live in ${country}`)
-} else {
-    console.log("find another country")
+function percentageOfWorld1(population) {
+    return(population/7900) * 100;
 }
 
-
-// switch 
-
-// switch (language) {
-//     case mandarin:
-//         console.log("Most number of native speakers!");
-//         break;
-//     case spanish:
-//         console.log("2nd place in number of native speakers");
-//         break;
-//     case english: 
-//         console.log("3rd place");
-//         break;
-//     case hindi:
-//         console.log("number 4");
-//         break;
-//     case arabic:
-//         console.log("5th most spoken language");
-//         break
-//     default:
-//         console.log("time to learn a language");
-// }
-
-// ternary operator 
-// if(`${population}` > 33){ 
-//     console.log(`${country}'s populaton is above average`);
-// } else {
-//     console.log(`${country}'s populaton is below average`);
-// }
-
+const germanyPer = percentageOfWorld1(83);
+const portugalPer = percentageOfWorld1(10);
+const finlandPer = percentageOfWorld1(51);
+console.log(germanyPer, portugalPer, finlandPer)
