db21825bf7e777e7e2bafaa53bfcede3804cfd71
Steven Boulom <sboulom@gmail.com>
2021-03-06 08:12:11 -0800

    different js functions retirement

--- script.js
+++ script.js
@@ -38,14 +38,38 @@
 // console.log(age1, age2)
 //arrow function
 
-const calcAge3 = birthYear => 2037 - birthYear;
-const age3 = calcAge3(1991);
-console.log(age3)
+// const calcAge3 = birthYear => 2037 - birthYear;
+// const age3 = calcAge3(1991);
+// console.log(age3)
 
-const yearsUntilRetirement = birthYear => {
-    const age = 2037 - birthYear;
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//     const age = 2037 - birthYear;
+//     const retirement = 65 - age;
+//     return `${firstName} retires in ${retirement} years`;
+// }
+
+// console.log(yearsUntilRetirement(1990, 'Steven'))
+
+// function cutFruitPieces(fruit){
+//     return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges){
+//     const applePieces = cutFruitPieces(apples)
+//     const orangePieces = cutFruitPieces(oranges)
+//     const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`
+//     return juice;
+
+// }
+// console.log(fruitProcessor(2, 3));
+
+const calcAge = function(birthYear){
+    return 2037 - birthYear;
+}
+const yearsUntilRetirement = function(birthYear, firstName) {
+    const age = calcAge(birthYear);
     const retirement = 65 - age;
-    return retirement;
+    return `${firstName} retires in ${retirement} years`;
 }
 
-console.log(yearsUntilRetirement(1990))
+yearsUntilRetirement(1991, 'Jonas');
