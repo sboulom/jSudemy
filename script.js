adb5e1ef881b8bad0ff2965a9dad9c88517e6d80
Steven Boulom <sboulom@gmail.com>
2021-03-15 14:09:13 -0700

    tip cal and challenge #4

--- script.js
+++ script.js
@@ -225,22 +225,58 @@
 // }
 // console.log(ages)
 
-const jonas = [
-    'Jonas',
-    'Schmedtmann',
-    2037 - 1991,
-    'teacher'
-    ['Michael', 'Peter', 'Steven']
-];
+// const jonas = [
+//     'Jonas',
+//     'Schmedtmann',
+//     2037 - 1991,
+//     'teacher'
+//     ['Michael', 'Peter', 'Steven']
+// ];
 
-for (let i = jonas.length -1; i >= 0; i--) {
-    console.log(i, jonas[i])
+// for (let i = jonas.length -1; i >= 0; i--) {
+//     console.log(i, jonas[i])
     
-}
+// }
 
-for (let exercise = 1; exercise < 4; exercise++){
-    console.log(`------starting exercise ${exercise}`);
-    for (let rep = 1; rep < 6; rep++){
-        console.log(`lifting weights rep ${rep}`)     
-    }
+// for (let exercise = 1; exercise < 4; exercise++){
+//     console.log(`------starting exercise ${exercise}`);
+//     for (let rep = 1; rep < 6; rep++){
+//         console.log(`lifting weights rep ${rep}`)     
+//     }
+// };
+
+// let dice = Math.trunc(Math.random() * 6) +1;
+// console.log(dice)
+
+// while(dice !== 6) {
+//     dice = Math.trunc(Math.random() * 6) +1;
+//     console.log(`you rolled a ${dice}`);
+//     if(dice === 6) {
+//         console.log(`over~~~~~~`)
+//     }
+// }
+
+//challenge #4
+
+const calcTip = function(bill){
+    return bill >= 50 && bill <= 300 ? bill * .15 : bill * .2 
+};
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = []; 
+
+for (let i = 0; i < bills.length; i++) {
+   const tip = calcTip(bills[i]);
+   tips.push(tip);
+   totals.push(tip + bills[i])
 }
+console.log( bills, tips, totals);
+
+const calcAverage = function(arr){
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i]; 
+    }
+    return sum / arr.length;
+}
+console.log(calcAverage(totals))
