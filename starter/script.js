4326af82090dbe9ebeb4715e21ea4c5187f73da8
Steven Boulom <sboulom@gmail.com>
2021-03-18 10:02:11 -0700

    calc ampitude

--- starter/script.js
+++ starter/script.js
@@ -5,7 +5,7 @@
 // const calcAge = birthYear => 2037 - birthYear;
 // console.log(calcAge(2018));
 
-const temperatures = [3. -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 const calcTempAmplitude = function(temps){
     let max = temps[0];
@@ -19,7 +19,29 @@ const calcTempAmplitude = function(temps){
         if(curTemp < min) min = curTemp;
     }
     console.log(max, min);
+    return max - min;
 }; 
 
-calcTempAmplitude([3, 7, 4, 1, 8]); 
-calcTempAmplitude(temperatures);
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
+
+const calcTempAmplitudeNew = function(t1, t2){
+
+    const temps = t1.concat(t2);
+    let max = temps[0];
+    let min = temps[0];
+    console.log(temps)
+
+    for (let i = 0; i < temps.length; i++){
+        const curTemp = temps[i];
+        if(typeof curTemp !== "number") continue;
+
+        if(curTemp > max) max = curTemp;
+        if(curTemp < min) min = curTemp;
+    }
+    console.log(max, min);
+    return max - min;
+}; 
+
+const amplitudeNew= calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
