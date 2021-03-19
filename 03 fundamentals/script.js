f578abbb7f881ca41d9eccf7e1409bf4fded566e
Steven Boulom <sboulom@gmail.com>
2021-03-19 00:48:00 -0700

    dom

--- 03 fundamentals/script.js
+++ 03 fundamentals/script.js
@@ -1,62 +1,62 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-// const x = 23;
-// const calcAge = birthYear => 2037 - birthYear;
-// console.log(calcAge(2018));
+const x = 23;
+const calcAge = birthYear => 2037 - birthYear;
+console.log(calcAge(2018));
 
-// const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
-// const calcTempAmplitude = function(temps){
-//     let max = temps[0];
-//     let min = temps[0];
+const calcTempAmplitude = function(temps){
+    let max = temps[0];
+    let min = temps[0];
 
-//     for (let i = 0; i < temps.length; i++){
-//         const curTemp = temps[i];
-//         if(typeof curTemp !== "number") continue;
+    for (let i = 0; i < temps.length; i++){
+        const curTemp = temps[i];
+        if(typeof curTemp !== "number") continue;
 
-//         if(curTemp > max) max = curTemp;
-//         if(curTemp < min) min = curTemp;
-//     }
-//     console.log(max, min);
-//     return max - min;
-// }; 
+        if(curTemp > max) max = curTemp;
+        if(curTemp < min) min = curTemp;
+    }
+    console.log(max, min);
+    return max - min;
+}; 
 
-// const amplitude = calcTempAmplitude(temperatures);
-// console.log(amplitude);
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
 
-// const calcTempAmplitudeNew = function(t1, t2){
+const calcTempAmplitudeNew = function(t1, t2){
 
-//     const temps = t1.concat(t2);
-//     let max = temps[0];
-//     let min = temps[0];
-//     console.log(temps)
+    const temps = t1.concat(t2);
+    let max = temps[0];
+    let min = temps[0];
+    console.log(temps)
 
-//     for (let i = 0; i < temps.length; i++){
-//         const curTemp = temps[i];
-//         if(typeof curTemp !== "number") continue;
+    for (let i = 0; i < temps.length; i++){
+        const curTemp = temps[i];
+        if(typeof curTemp !== "number") continue;
 
-//         if(curTemp > max) max = curTemp;
-//         if(curTemp < min) min = curTemp;
-//     }
-//     console.log(max, min);
-//     return max - min;
-// }; 
+        if(curTemp > max) max = curTemp;
+        if(curTemp < min) min = curTemp;
+    }
+    console.log(max, min);
+    return max - min;
+}; 
 
-// const amplitudeNew= calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
-// console.log(amplitudeNew);
+const amplitudeNew= calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
 
-// const measureKelvin = function (){
-//     const measurement = {
-//         type: "temp",
-//         unit: "celsius",
-//         value: Number(prompt("Degrees Celsius"))
-//     }
-//     const kelvin = measurement.value + 273;
-//     return kelvin;
+const measureKelvin = function (){
+    const measurement = {
+        type: "temp",
+        unit: "celsius",
+        value: Number(prompt("Degrees Celsius"))
+    }
+    const kelvin = measurement.value + 273;
+    return kelvin;
 
-// }
-// console.log(measureKelvin());
+}
+console.log(measureKelvin());
 
 
 const data1 = [17, 21, 23];
