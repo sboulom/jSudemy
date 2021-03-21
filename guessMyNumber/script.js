a03153d72460b1638f809528f52fef1a9bb4d3f7
Steven Boulom <sboulom@gmail.com>
2021-03-20 23:41:20 -0700

    game functionality

--- guessMyNumber/script.js
+++ guessMyNumber/script.js
@@ -9,11 +9,11 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
-const secretNumber = Math.trunc(Math.random() * 20) +1;
+let secretNumber = Math.trunc(Math.random() * 20) +1;
 let score = 20;
 
 //showing the secret number for now.....
-document.querySelector('.number').textContent = secretNumber;
+
 
 document.querySelector('.check').addEventListener('click', function(){
     // console.log(document.querySelector('.guess').value);
@@ -26,8 +26,11 @@ document.querySelector('.check').addEventListener('click', function(){
         //when player wins
     } else if (guess === secretNumber){
         document.querySelector('.message').textContent = '👍 Correct Number';
-
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
         //guess is too high
+
     } else if (guess > secretNumber){
         if(score > 1){
         document.querySelector('.message').textContent = '🪂 Too High';
@@ -49,9 +52,17 @@ document.querySelector('.check').addEventListener('click', function(){
         } else {
         document.querySelector('.message').textContent = 'YOU LOSE......'
         document.querySelector('.score').textContent = 0;
-        }
-        
+        }   
     }
-    
-
 });
+
+document.querySelector('.again').addEventListener('click', function(){
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) +1;
+    document.querySelector('.message').textContent = 'Start Guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+})
