995f237adb55c98aae45c6d22c05e18967380538
Steven Boulom <sboulom@gmail.com>
2021-03-25 09:02:40 -0700

    test

--- modal/starter/script.js
+++ modal/starter/script.js
@@ -7,9 +7,8 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
 for(let i = 0; i < btnsOpenModal.length; i++){
-    console.log(btnsOpenModal[i].textContent);
+    btnsOpenModal[i].addEventListener('click', function(){
+        console.log('button clicked')
+    });
 
 }
-
-
-console.log("hello")
