5012825d3b63403754d566661b0f710e20a01fc5
Steven Boulom <sboulom@gmail.com>
2021-03-26 08:54:39 -0700

    switch

--- pigGame/starter/script.js
+++ pigGame/starter/script.js
@@ -4,7 +4,30 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
+//starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');  
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function(){
+    //1. Generating random dice roll
+    const dice = Math.trunc(Math.random() * 6) +1;
+
+    //2. Display dice
+    diceEl.classList.remove('hidden');
+    switch (dice) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+ 
+    //3. Check for rolled 1, if true, switch player
+})
