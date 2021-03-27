8438cdbf62cb880506c839547feb944e6eb5439b
Steven Boulom <sboulom@gmail.com>
2021-03-27 08:11:35 -0700

    added reset button function to start game

--- pigGame/starter/script.js
+++ pigGame/starter/script.js
@@ -8,21 +8,34 @@ const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
-
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 //starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');  
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+
+const init = function(){
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score1El.textContent = 0;
+    score0El.textContent = 0;
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    
+    diceEl.classList.add('hidden');  
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+};
+
+init();
 
 const switchPlayer = function(){
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -61,7 +74,7 @@ btnHold.addEventListener('click', function(){
         scores[activePlayer] += currentScore;
         document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
         //2. check score >= 100 
-        if(scores[activePlayer] >= 20){
+        if(scores[activePlayer] >= 100){
             //finish the game
             playing = false;
            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
@@ -74,3 +87,5 @@ btnHold.addEventListener('click', function(){
     }
 
 });
+
+btnNew.addEventListener('click', init);
