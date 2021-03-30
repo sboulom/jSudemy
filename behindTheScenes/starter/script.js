3c8dbdfd2e9af581cd5408a259f65ad2aea6c99f
Steven Boulom <sboulom@gmail.com>
2021-03-29 19:45:12 -0700

    scope

--- behindTheScenes/starter/script.js
+++ behindTheScenes/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function calcAge(birthYear){
+    const age = 2037 - birthYear;
+    function printAge(){
+        const outPut = `You are ${age}, born in ${birthYear}`;
+        console.log(outPut);
+        if(birthYear >= 1981 && birthYear <= 1996){
+            const str = `You are a millenial ${firstName}`
+            console.log(str);
+        }
+    }
+    printAge();
+    return age;
+}
+
+const firstName = 'Steven';
+calcAge(1991);
