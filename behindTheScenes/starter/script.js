2a575f6dcea0ecebbbe34911de4b4d5d1c1066ee
Steven Boulom <sboulom@gmail.com>
2021-04-01 08:43:33 -0700

    section 9 practice

--- behindTheScenes/starter/script.js
+++ behindTheScenes/starter/script.js
@@ -1,18 +1,22 @@
 'use strict';
 
-function calcAge(birthYear){
-    const age = 2037 - birthYear;
-    function printAge(){
-        const outPut = `You are ${age}, born in ${birthYear}`;
-        console.log(outPut);
-        if(birthYear >= 1981 && birthYear <= 1996){
-            const str = `You are a millenial ${firstName}`
-            console.log(str);
-        }
-    }
-    printAge();
-    return age;
-}
+// function calcAge(birthYear){
+//     const age = 2037 - birthYear;
+//     function printAge(){
+//         const outPut = `You are ${age}, born in ${birthYear}`;
+//         console.log(outPut);
+//         if(birthYear >= 1981 && birthYear <= 1996){
+//             const str = `You are a millenial ${firstName}`
+//             console.log(str);
+//         }
+//         console.log(this);
+//     }
+//     printAge();
+//     return age;
+// }
 
-const firstName = 'Steven';
-calcAge(1991);
+// const firstName = 'Steven';
+// calcAge(1991);
+
+
+//  
