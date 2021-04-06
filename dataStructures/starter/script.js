0e589a6213741fa4e81cc9b835a451f61b1aca03
Steven Boulom <sboulom@gmail.com>
2021-04-05 20:32:41 -0700

    destructuring array

--- dataStructures/starter/script.js
+++ dataStructures/starter/script.js
@@ -8,9 +8,15 @@ const restaurant = {
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 };
 
-const arr = [2,3,4]
-const a = arr[0]
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
 
 
-const [first, second] = restaurant.categories;
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+const [first, , , second]= restaurant.categories;
 console.log(first, second);
