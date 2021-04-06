1afddea5eadced2051f3816769874c70b1c33900
Steven Boulom <sboulom@gmail.com>
2021-04-05 20:24:57 -0700

    initial files

--- dataStructures/starter/script.js
+++ dataStructures/starter/script.js
@@ -6,19 +6,11 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
 };
+
+const arr = [2,3,4]
+const a = arr[0]
+
+
+const [first, second] = restaurant.categories;
+console.log(first, second);
