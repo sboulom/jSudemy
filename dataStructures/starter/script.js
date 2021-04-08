47e63d187d29ffe86d305ef0df2e1c43034f1295
Steven Boulom <sboulom@gmail.com>
2021-04-08 07:47:47 -0700

    destructuring:

--- dataStructures/starter/script.js
+++ dataStructures/starter/script.js
@@ -6,17 +6,51 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11, 
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+
+  order: function(starterIndex, mainIndex){
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  }
 };
 
-const arr = [2,3,4];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
+const {name, openingHours, categories} = restaurant;
+console.log(name, openingHours, categories); 
 
 
-const [x, y, z] = arr;
-console.log(x, y, z);
-console.log(arr);
+// const arr = [2,3,4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
 
-const [first, , , second]= restaurant.categories;
-console.log(first, second);
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+// console.log(arr);
+
+// let [main, , secondary]= restaurant.categories;
+
+// [main, secondary] = [secondary, main]
+// console.log(main, secondary);
+
+// console.log(restaurant.order(2, 0));
+
+// const nested = [2, 4, [5, 6]];
+// // const [i, ,j] = nested;
+// // console.log(i, j);
+// const [i, , [j, k]] = nested
+// console.log(i, j, k);
+
+// const [p, q, r] =[8, 9]
+// console.log(p, q, r);
