fd5fd97173d48aa6d302ae565a288a467c72718a
Steven Boulom <sboulom@gmail.com>
2021-04-16 08:50:17 -0700

    checking answers

--- pollChallenge/script.js
+++ pollChallenge/script.js
@@ -9,29 +9,34 @@ const poll = {
     // This generates [0, 0, 0, 0]. More in the next section 😃
     answers: new Array(4).fill(0),
     registerNewAnswer() {
-      // Get answer
+     //get answer
       const answer = Number(
         prompt(
           `${this.question}\n${this.options.join('\n')}\n(Write option number)`
         )
       );
-      console.log(answer);
-  
-      // Register answer
+        console.log(answer);
+
+      // Register the answer
       typeof answer === 'number' &&
         answer < this.answers.length &&
         this.answers[answer]++;
-  
-      this.displayResults();
-      this.displayResults('string');
+          
+       console.log(this.answers); 
+      // this.displayResults();
+      // this.displayResults('string');
     },
   
-    displayResults(type = 'array') {
-      if (type === 'array') {
-        console.log(this.answers);
-      } else if (type === 'string') {
-        // Poll results are 13, 2, 4, 1
-        console.log(`Poll results are ${this.answers.join(', ')}`);
-      }
-    },
+    // displayResults(type = 'array') {
+    //   if (type === 'array') {
+    //     console.log(this.answers);
+    //   } else if (type === 'string') {
+    //     // Poll results are 13, 2, 4, 1
+    //     console.log(`Poll results are ${this.answers.join(', ')}`);
+    //   }
+    // },
   };
+
+  // poll.registerNewAnswer();
+
+document.querySelector('.poll')
