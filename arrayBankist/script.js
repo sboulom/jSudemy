8bb6394d52ee82c8ed35375dcba3edf78451bfb5
Steven Boulom <sboulom@gmail.com>
2021-04-22 08:45:25 -0700

    insertAdjacent

--- arrayBankist/script.js
+++ arrayBankist/script.js
@@ -61,6 +61,22 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+
+
+const displayMovements = function(movements){
+  movements.forEach(function(mov, i){
+    const type = mov > 0 ? 'deposit' : 'widthdrawl'
+
+    const html = `
+    <div class="movements__row">
+     <div class="movements__type movements__type--${type}">${i +1} ${type}</div>
+     <div class="movements__value">${mov}</div>
+    </div>`
+
+    containerMovements.insertAdjacentHTML('afterbegin')
+  })
+}
+displayMovements(account1.movements)
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -99,6 +115,7 @@ for( const movement of movements){
     console.log(`you widthdrew ${Math.abs(movement)}`);
   }
 }
+
 console.log('*********************');
 movements.forEach(function(movement, index, arrayb){
   if(movement > 0){
